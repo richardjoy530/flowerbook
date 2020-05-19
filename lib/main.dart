94021e5d8febc526abc6d12b618c93a3db88b3ed
Richard Joy <richardjoy530@gmail.com>
2020-05-19 12:40:08 +0530

    Added images, and layout skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
-        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
@@ -29,6 +27,8 @@ class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   AnimationController _controller;
 
+  get inputType => null;
+
   @override
   void initState() {
     super.initState();
@@ -43,8 +43,133 @@ class _HomePageState extends State<HomePage>
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset(
+              'images/path.png',
+              width: MediaQuery.of(context).size.width,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.more_vert,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      'Flowers',
+                      style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'School',
+                      style: TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xffffffff).withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        padding: EdgeInsets.only(left: 20),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              hintText: 'Search', border: InputBorder.none),
+                        ),
+                      ),
+                      IconButton(
+                          icon: Icon(Icons.photo_camera), onPressed: null)
+                    ],
+                  ),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(10),
+                          ),
+                          child: Container(
+                            padding: EdgeInsets.only(left: 15),
+                            color: Color(0xfff1f5f8),
+                            height: 200,
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Image.asset(
+                                    'images/monstra.jpg',
+                                    fit: BoxFit.fitWidth,
+                                    width: 200,
+                                  ),
+                                ),
+                                Align(
+                                  alignment: Alignment.topLeft,
+                                  child: IconButton(
+                                      icon: Icon(Icons.favorite_border),
+                                      onPressed: null),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        'Flowers',
+                                        style: TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Row(children: <Widget>[CircleAvatar(backgroundImage:)],)
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
